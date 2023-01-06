8b168e9d8ad86e978053563113d62aca0ac09b50
Atila jr <atilla.junior33@gmail.com>
2023-01-06 16:39:02 -0300

    HashCod

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    // HashCode Método que retorna um número inteiro representando um código
+    //gerado a partir das informações do objeto
+    System.out.println("=============================");
+
+    String a = "Maria";
+    String b = "Alex";
+
+    System.out.println(a.hashCode());
+    System.out.println(b.hashCode());
+
+    System.out.println("=============================");
+  }
+}
